04c842ebf43da12fb9fb32e0232b47e78d6b1ad4
Nikola Kelava <nikola.kelava94@gmail.com>
2021-11-30 17:39:30 +0100

    Rewrote simple calc with switch

--- pages/javascript/script.js
+++ pages/javascript/script.js
@@ -1,14 +1,30 @@
+// function calculate(first, second, op) {
+//     if( op === '+') 
+//         return first + second;
+//     else if( op === '-')
+//         return first - second;
+//     else if( op === '*')
+//         return first * second;
+//     else if( op === '/' && second !== 0)
+//         return first / second;
+//     else
+//         return "Invalid input.\nMake sure that you input only numbers, you don't divide with 0 and used correct operation.";
+// }
+
 function calculate(first, second, op) {
-    if( op === '+') 
-        return first + second;
-    else if( op === '-')
-        return first - second;
-    else if( op === '*')
-        return first * second;
-    else if( op === '/' && second !== 0)
-        return first / second;
-    else
-        return "Invalid input.\nMake sure that you input only numbers, you don't divide with 0 and used correct operation.";
+    switch (op) {
+        case '+':
+            return first + second;
+        case '-':
+            return first - second;
+        case '*':
+            return first * second;
+        case '/':
+            if (second !== 0)
+                return first / second;
+        default:
+            return "Invalid input.\nMake sure that you input only numbers, you don't divide with 0 and used correct operation.";
+    }
 }
 
 var firstNumber = Number(prompt("Enter first number: "));
