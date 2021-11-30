3e375be1565231b34546e952c29fb8766666e07b
Nikola Kelava <nikola.kelava94@gmail.com>
2021-11-30 21:42:04 +0100

    Recode calc program with calculator as an object

--- pages/javascript/script.js
+++ pages/javascript/script.js
@@ -31,19 +31,37 @@ var firstNumber = Number(prompt("Enter first number: "));
 var secondNumber = Number(prompt("Enter second number: "));
 var operation = prompt("Enter algorithmic operation (+, -, *, /): ");
 
-// alert("Result is: " + calculate(firstNumber, secondNumber, operation));
+// var result = function(first, second, op) {
+//     if( op === '+') 
+//     return first + second;
+//     else if( op === '-')
+//     return first - second;
+//     else if( op === '*')
+//     return first * second;
+//     else if( op === '/' && second !== 0)
+//     return first / second;
+//     else
+//     return "Invalid input.\nMake sure that you input only numbers, you don't divide with 0 and used correct operation.";
+// }
 
-var result = function(first, second, op) {
-    if( op === '+') 
+var calculator = {
+    version: "1.0",
+    name: "Casio",
+    operations: ['+', '-', '*', '/'],
+    calculate: function(first, second, op) {
+        if( op === '+') 
         return first + second;
-    else if( op === '-')
+        else if( op === '-')
         return first - second;
-    else if( op === '*')
+        else if( op === '*')
         return first * second;
-    else if( op === '/' && second !== 0)
+        else if( op === '/' && second !== 0)
         return first / second;
-    else
+        else
         return "Invalid input.\nMake sure that you input only numbers, you don't divide with 0 and used correct operation.";
+    },
 }
 
-alert("Result is: " + result(firstNumber, secondNumber, operation));
+// alert("Result is: " + calculate(firstNumber, secondNumber, operation));
+// alert("Result is: " + result(firstNumber, secondNumber, operation));
+alert("Result calculated by " + calculator.name + calculator.version + " is: " + calculator.calculate(firstNumber, secondNumber, operation));
