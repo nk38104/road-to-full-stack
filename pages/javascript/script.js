4b4f5796fbedb84c37bd5d8a8bbf6be8550d0e7b
Nikola Kelava <nikola.kelava94@gmail.com>
2021-11-30 17:40:32 +0100

    Rewrote calc program as function expression

--- pages/javascript/script.js
+++ pages/javascript/script.js
@@ -11,24 +11,39 @@
 //         return "Invalid input.\nMake sure that you input only numbers, you don't divide with 0 and used correct operation.";
 // }
 
-function calculate(first, second, op) {
-    switch (op) {
-        case '+':
-            return first + second;
-        case '-':
-            return first - second;
-        case '*':
-            return first * second;
-        case '/':
-            if (second !== 0)
-                return first / second;
-        default:
-            return "Invalid input.\nMake sure that you input only numbers, you don't divide with 0 and used correct operation.";
-    }
-}
+// function calculate(first, second, op) {
+//     switch (op) {
+//         case '+':
+//             return first + second;
+//         case '-':
+//             return first - second;
+//         case '*':
+//             return first * second;
+//         case '/':
+//             if (second !== 0)
+//                 return first / second;
+//         default:
+//             return "Invalid input.\nMake sure that you input only numbers, you don't divide with 0 and used correct operation.";
+//     }
+// }
 
 var firstNumber = Number(prompt("Enter first number: "));
 var secondNumber = Number(prompt("Enter second number: "));
 var operation = prompt("Enter algorithmic operation (+, -, *, /): ");
 
-alert("Result is: " + calculate(firstNumber, secondNumber, operation));
+// alert("Result is: " + calculate(firstNumber, secondNumber, operation));
+
+var result = function(first, second, op) {
+    if( op === '+') 
+        return first + second;
+    else if( op === '-')
+        return first - second;
+    else if( op === '*')
+        return first * second;
+    else if( op === '/' && second !== 0)
+        return first / second;
+    else
+        return "Invalid input.\nMake sure that you input only numbers, you don't divide with 0 and used correct operation.";
+}
+
+alert("Result is: " + result(firstNumber, secondNumber, operation));
