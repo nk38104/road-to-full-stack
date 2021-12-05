989ad7fcccd57b8f85751d3989faa2a6a7ee60be
Nikola Kelava <nikola.kelava94@gmail.com>
2021-12-05 22:40:28 +0100

    Add checkbox toggle task status functionality

--- pages/javascript_section/projects/toDo/toDo.js
+++ pages/javascript_section/projects/toDo/toDo.js
@@ -1,39 +1,61 @@
-var button = document.getElementById("btn-add");
-var userInput = document.getElementById("task-input");
-var ul = document.querySelector("#task-list");
+
 
 function checkInputLength() {
     return userInput.value.length;
 }
 
+function toggleTaskStatus(event) {
+    var checkboxTarget = event.currentTarget;
+    var parent = checkboxTarget.parentNode;
 
-function addListItemChildren(li) {
-    var  checkbox = document.createElement("input");
-    var button = document.createElement("button");
+    if(checkboxTarget.checked) {
+        parent.classList.add("done");
+    }
+    else {
+        parent.classList.remove("done");
+    }
+
+    checkboxTarget.addEventListener("click", toggleTaskStatus);
+}
+
+function setTaskCheckbox(checkbox) {
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.style.height = "18px";
+    checkbox.style.width = "18px";
+
+    checkbox.addEventListener("click", toggleTaskStatus);
+}
+
+function setTaskDeleteButton(button) {
     var image = document.createElement("img");
+
+    button.setAttribute("type", "submit");
+    button.setAttribute("class", "btn-delete");
+    button.style.width = "50px";
+    button.style.height = "35px";
+
+    image.setAttribute("src", "./images/delete-icon.ico");
+    button.appendChild(image);
+}
+
+function addNewListItemChildren(li) {
+    var checkbox = document.createElement("input");
+    var deleteButton = document.createElement("button");
+
+    setTaskCheckbox(checkbox);
+    setTaskDeleteButton(deleteButton);
+
+    li.appendChild(checkbox);
+    li.appendChild(document.createTextNode(userInput.value));
+    li.appendChild(deleteButton);
 }
 
 function createListElement() {
     var li = document.createElement("li");
-    var  checkbox = document.createElement("input");
-    var button = document.createElement("button");
-    var image = document.createElement("img");
-    
-    addListItemChildren(li);
-    
     li.setAttribute("class", "task-item");
-
-    checkbox.setAttribute("type", "checkbox");
-    button.setAttribute("type", "submit");
-    button.setAttribute("class", "btn-delete");
-    image.setAttribute("src", "./images/delete-icon.ico");
     
-    button.appendChild(image);
-
-    li.appendChild(checkbox);
-    li.appendChild(document.createTextNode(userInput.value));
-    li.appendChild(button);
-
+    addNewListItemChildren(li);
+    
     ul.appendChild(li);
 
     userInput.value = "";
@@ -50,6 +72,14 @@ function addTaskAfterKeyPress(event) {
         createListElement();
     }
 }
+
+// ---------------------------------------
+// ---------------- MAIN  ----------------
+// ---------------------------------------
+
+var button = document.getElementById("btn-add");
+var userInput = document.getElementById("task-input");
+var ul = document.querySelector("#task-list");
 
 button.addEventListener("click", addTaskAfterClick);
 userInput.addEventListener("keypress", addTaskAfterKeyPress);
