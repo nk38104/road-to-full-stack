4c193d93b381d3e0fec490ae54479558d3cee2a1
Nikola Kelava <nikola.kelava94@gmail.com>
2021-12-17 21:52:37 +0100

    Add third practice problem solved

--- pages/javascript_section/exercises/js_practice.js
+++ pages/javascript_section/exercises/js_practice.js
@@ -1,8 +1,10 @@
-// Question 1: 
+//-------------------------------------------------------------------------------------------------------------------------------
+// QUESTION 1: 
 // Clean the room function: given an input of [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20],
 // make a function that organizes these into individual array that is ordered. 
 // For example answer(ArrayFromAbove) should return: [[1,1,1,1],[2,2,2], 4,5,10,[20,20],391,392,591].
 // Bonus: Make it so it organizes strings differently from number types. i.e. [1, "2", "3", 2] should return [[1,2], ["2", "3"]];
+//-------------------------------------------------------------------------------------------------------------------------------
 
 function question1_v2(array) {
     let sortedArray = [...array.sort((a, b) => a - b)];
@@ -47,10 +49,12 @@ console.log("Version 1:", question1(array.flat(Infinity)));
 console.log("Version 2:", question1_v2(array.flat(Infinity)));
 
 
-// Question 2:
+//---------------------------------------------------------------------------------------------------------------
+// QUESTION 2:
 // Write a javascript function that takes an array of numbers and a target number.
 // The function should find two different numbers in the array that, when added together, give the target number.
 // For example: answer([1,2,3], 4)should return [1,3]
+//---------------------------------------------------------------------------------------------------------------
 
 function question2_v2(numbers, target) {
     for(const [index, num] of numbers.entries()) {
@@ -76,8 +80,68 @@ function question2(numbers, target) {
 const array2 = [[ 1, 2, 3, -1, 10, -56, -3 ], 0];
 console.log("\nQUESTION 2");
 console.log("Version 1:", question2(array2[0], array2[1]));
-console.log("Version 2:", question2(array2[0], array2[1]));
+console.log("Version 2:", question2_v2(array2[0], array2[1]));
 
-// Question 3:
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------
+// QUESTION 3:
 // Write a function that converts HEX to RGB.
 // Then Make that function auto-dect the formats so that if you enter HEX color format it returns RGB and if you enter RGB color format it returns HEX.
+// ----------------------------------------------------------------------------------------------------------------------------------------------------
+
+const checkIfNumber = num => typeof num === "number";
+const checkRGBValues = num => num < 256 && num > 0;
+
+let hexColor = "#428ef5";                 // hex format - # + 3 or 6 digit string
+const rgbColor = convertToRGB(hexColor);    // rgb format - 3 numbers from 0 to 255 representing red, green, blue
+
+
+// HEX --> RGB
+function convertToRGB(hexColor) {
+
+    if (hexColor.length != 7 && hexColor.length != 4) {
+        return null;
+    }
+    
+    let rgbColor = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexColor);
+
+    return rgbColor ? [parseInt(rgbColor[1], 16), parseInt(rgbColor[2], 16), parseInt(rgbColor[3], 16)] : null;
+}
+
+
+// RGB --> HEX
+function componentToHex(component) {
+    let hexComponent = component.toString(16);
+    return (hexComponent.length === 1) ? "0" + hexComponent : hexComponent;
+}
+
+// This function can be called successfully in 2 ways
+// 1. convertToHex(number, number, number); ---> Function accepts 3 number parameters representing red, green and blue values (0 - 255)
+// 1. convertToHex(array);                  ---> Function accpets array consisting of 3 numbers representing red, green, blue values (0 - 255)
+function convertToHex(red, green, blue) {
+
+    if(arguments.length != 3 && arguments.length != 1) {
+        return "Invalid arguments.";
+    }
+
+    if(arguments.length === 1){
+        if(arguments[0].length != 3 || !Array.isArray(arguments[0])) {
+            return "Invalid arguments.";
+        }
+    }
+
+    let args = (arguments.length === 3) ? [...arguments] : arguments[0];
+
+    return (args.every(checkIfNumber || checkRGBValues)) ? `#${componentToHex(args[0])}${componentToHex(args[1])}${componentToHex(args[2])}` : "Invalid arguments.";
+}
+
+console.log("\nQUESTION 3");
+console.log(`Hex ${hexColor} ==> rgb(${rgbColor})`);
+
+
+if((hexColor = rgbColor)) {
+    hexColor = convertToHex(rgbColor[0], rgbColor[1], rgbColor[2]);
+    // hexColor = convertToHex(rgbColor);
+}
+
+console.log(`rgb(${rgbColor}) ==> ${hexColor}`);
