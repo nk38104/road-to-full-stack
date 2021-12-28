a989fd852040413e42846519beb8f84a9a8554c1
Nikola Kelava <nikola.kelava94@gmail.com>
2021-12-28 23:59:29 +0100

    Add css for new component file

--- pages/react_section/projects/robofriends/src/Greeting.js
+++ pages/react_section/projects/robofriends/src/Greeting.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import './Greeting.css'
 
 class Greeting extends Component {
     render() {
         return (
             <div>
                 <h1>Hello, User!</h1>
-                <p>Welcome to my site</p>
+                <p>Welcome to my site </p>
             </div>
         );
     }
