b56f9e56e65cca5997c79d62ed2c24d651e8d0c5
Nikola Kelava <nikola.kelava94@gmail.com>
2021-12-29 18:54:27 +0100

    Clean unnecessary files from init project for new start

--- pages/react_section/projects/robofriends/src/index.js
+++ pages/react_section/projects/robofriends/src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Greeting from './Greeting'
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-ReactDOM.render(<Greeting/>, document.getElementById("root"));
+ReactDOM.render(<h1>Hello!</h1>, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
