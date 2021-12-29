b1dddd8d5e8e73141dc383834dc6feaf8e28e074
Nikola Kelava <nikola.kelava94@gmail.com>
2021-12-29 20:55:30 +0100

    Add strict mode and change single component call pattern so far to fragment

--- pages/react_section/projects/robofriends/src/index.js
+++ pages/react_section/projects/robofriends/src/index.js
@@ -8,11 +8,13 @@ import { robots }from './robots';
 
 
 ReactDOM.render(
-    <div>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    </div>
+    <React.StrictMode>
+        <>
+            <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+            <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+            <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+        </>
+    </React.StrictMode>
     , document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
