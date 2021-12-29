8292358526a25abce3532713a595dcb95ff9a904
Nikola Kelava <nikola.kelava94@gmail.com>
2021-12-29 19:06:00 +0100

    Add init card component

---
+++ pages/react_section/projects/robofriends/src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img alt="profile_picture" src="https://robohash.org/test?200x200"/>
+            <div>
+                <h2>Firstnam Lastname</h2>
+                <p>email@gmail.com</p>
+            </div>
+        </div>
+    );
+ }
+
+export default Card;
