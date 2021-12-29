2f980bbff9a37f9c7208543dc002d71db39124b8
Nikola Kelava <nikola.kelava94@gmail.com>
2021-12-29 19:30:49 +0100

    Add default robot friends, add couple of cards with robot info

--- pages/react_section/projects/robofriends/src/index.js
+++ pages/react_section/projects/robofriends/src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import Card from './Card';
+import { robots }from './robots';
 
 
-ReactDOM.render(<Card/>, document.getElementById("root"));
+ReactDOM.render(
+    <div>
+        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+    </div>
+    , document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
