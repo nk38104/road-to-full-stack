82796b37e8cd9b69e6763c9447e38850fb27840a
Nikola Kelava <nikola.kelava94@gmail.com>
2021-12-30 20:50:29 +0100

    Add search bar functionality

--- pages/react_section/projects/robofriends/src/SearchBox.js
+++ pages/react_section/projects/robofriends/src/SearchBox.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 
-const SearchBox = () => {
+const SearchBox = ({ searchChange }) => {
     return (
         <div>
-            <input className='pa3 ba b--green bg-lightest-blue '
-                type='search' 
-                placeholder='Search robots...'/>
+            <input 
+                className = 'pa3 ba b--green bg-lightest-blue '
+                type = 'search' 
+                placeholder = 'Search robots...'
+                onChange = {searchChange}/>
         </div>
     );
 }
