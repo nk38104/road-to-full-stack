55cb2a5a28a0c97e88fcef6e22b027ac3411477b
Nikola Kelava <nikola.kelava94@gmail.com>
2022-01-01 23:51:22 +0100

    Modify code so it is a bit cleaner and structure files into folder structure

--- pages/react_section/projects/robofriends/src/index.js
+++ pages/react_section/projects/robofriends/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+import App from './containers/App';
 import 'tachyons';
-import App from './App';
 
 
 ReactDOM.render(
