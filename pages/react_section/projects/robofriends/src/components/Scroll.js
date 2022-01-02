518529b82cd38906664b0dc7f3dac11f3d9e0d95
Nikola Kelava <nikola.kelava94@gmail.com>
2022-01-02 21:19:25 +0100

    Style page a bit

--- pages/react_section/projects/robofriends/src/components/Scroll.js
+++ pages/react_section/projects/robofriends/src/components/Scroll.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', height: '1024px'}}>
+        <div style={{overflowY: 'scroll', height: '900px', paddingBottom: "225px"}}>
             {props.children}
         </div>
     );
--- pages/react_section/projects/robofriends/src/components/SearchBox.js
+++ pages/react_section/projects/robofriends/src/components/SearchBox.js
@@ -5,7 +5,7 @@ const SearchBox = ({ searchChange }) => {
     return (
         <div>
             <input 
-                className = 'pa3 ba b--green bg-lightest-blue '
+                className = 'ma5 pa3 ba b--green bg-lightest-blue br2 w-third'
                 type = 'search' 
                 placeholder = 'Search robots...'
                 onChange = {searchChange}/>
