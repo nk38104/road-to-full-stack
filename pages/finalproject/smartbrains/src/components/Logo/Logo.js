00d814c03338185fe2e4754895bcf9e0a739e3c1
Nikola Kelava <nikola.kelava94@gmail.com>
2022-01-05 22:38:08 +0100

    Add logo component

---
+++ pages/finalproject/smartbrains/src/components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import logoImage from './logo_image.png';
+import './Logo.css';
+
+
+const Logo = () => {
+    return (
+        <div className="ma4 mt0">
+            <Tilt className="tilt br2 shadow-2" options={{ max: 35 }}>
+                <div className="tilt-inner pa3">
+                    <img className="logo-img" alt="logo" src={logoImage}/>
+                </div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
