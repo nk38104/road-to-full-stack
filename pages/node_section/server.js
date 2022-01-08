d17b9318eff5420b3a327e422fd84cc5c44d8575
Nikola Kelava <nikola.kelava94@gmail.com>
2022-01-08 22:02:45 +0100

    Modify server to return html file

--- pages/node_section/server.js
+++ pages/node_section/server.js
@@ -2,19 +2,6 @@ const express = require('express');
 
 
 const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-
-app.get("/", (req, res) => {
-    res.send("Home page");
-});
-
-app.get("/profiles", (req, res) => {
-    res.send("Profile page");
-});
-
-app.post("/profiles", (req, res) => {
-    res.send(req.body);
-});
+app.use(express.static(`${__dirname}/public`));
 
 app.listen(3000);
