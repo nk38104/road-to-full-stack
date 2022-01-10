2c9197d700c0bafd2aebc5be31d8c0b58404c831
Nikola Kelava <nikola.kelava94@gmail.com>
2022-01-10 19:43:04 +0100

    Add basic sing in endpoint

--- pages/finalproject/smartbrains/backend/api/server.js
+++ pages/finalproject/smartbrains/backend/api/server.js
@@ -12,10 +12,42 @@ const express = require("express");
 */
 
 const app = express();
+app.use(express.json());
+
+const database = {
+    users: [
+        {
+            id:         "123",
+            username:   "John",
+            password:   "john123",
+            email:      "john@gmail.com",
+            entries:    0,
+            joined:     new Date(),
+        },
+        {
+            id:         "124",
+            username:   "Liza",
+            password:   "liza123",
+            email:      "liza@gmail.com",
+            entries:    0,
+            joined:     new Date(),
+        }
+    ],
+}
 
 app.get("/", (req, resp) => {
     resp.send("Hello, there. Welcome to Home page!");
-})
+});
+
+app.post("/signin", (req, resp) => {
+    if(req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+            resp.json("You are logged in.");
+    }
+    else {
+        resp.status(400).json("Error loggin in.");
+    }
+});
 
 app.listen(3000, () => {
     console.log("App is running on port 3000.");
