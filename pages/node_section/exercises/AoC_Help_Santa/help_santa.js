3f26b80d9e414e8130b4a0ba09b361d0ff372291
Nikola Kelava <nikola.kelava94@gmail.com>
2022-01-10 03:38:38 +0100

    Add second part of the task and modify solution

--- pages/node_section/exercises/AoC_Help_Santa/help_santa.js
+++ pages/node_section/exercises/AoC_Help_Santa/help_santa.js
@@ -2,27 +2,50 @@
 const fs = require("fs");
 
 
-function GetPath() {
-    const path = fs.readFileSync(`${__dirname}\\path.txt`, err => {
-        if(err) {
-            console.log(error);
-        }
-    });
-
-    return path.toString();
+function CheckIfEnteredBasement(instructionId, santa) {
+    if((santa.onFloor < 0) && !santa.wentToBasement) {
+        console.log(`Position on path that made Santa enter the basement is ${++instructionId}.`);
+        santa.wentToBasement = true;
+    }
 }
 
-const path = GetPath();
-let goUpCounter = goDownCounter = 0;
-
-for (const direction of path) {
+function GoToFloor(direction, instructionId, santa) {
     if(direction === '(') {
-        ++goUpCounter;
-        continue;
+        santa.onFloor += 1;
     }
     if(direction === ')') {
-        ++goDownCounter;
+        santa.onFloor -= 1;
+        CheckIfEnteredBasement(instructionId, santa);
     }
-};
+}
 
-console.log(`Santa has to go on floor n.${goUpCounter - goDownCounter}!`);
+function GetInstructions() {
+    const instructions = fs.readFileSync(`${__dirname}\\instructions.txt`);
+
+    return instructions.toString();
+}
+
+function DeliverPresents() {
+    try { 
+        const instructions = GetInstructions();
+        const instructionCount = instructions.length;
+    
+        const santa = {
+            wentToBasement: false,
+            onFloor: 0,
+        }
+
+        for (let id = 0; id < instructionCount; ++id) {
+            if (!"()".includes(instructions[id])) throw new Error("Error: Invalid direction in the instructions.");
+
+            GoToFloor(instructions[id], id, santa);
+        };
+
+        return `Instructions took Santa on floor n.${santa.onFloor}!`
+    } catch (err) {
+        return err.message;
+    }
+}
+
+
+console.log(DeliverPresents());
