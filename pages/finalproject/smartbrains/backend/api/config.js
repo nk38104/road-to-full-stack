95f674bdad88eb9ba55c2556fa7c8c0ae428e5c9
Nikola Kelava <nikola.kelava94@gmail.com>
2022-01-17 22:08:18 +0100

    Delete moved files (moved react and final project to separate repos)

--- pages/finalproject/smartbrains/backend/api/config.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-module.exports = {
-  PORT: process.env.PORT,
-  CLARIFAI_API_KEY: process.env.CLARIFAI_API_KEY,
-  DATABASE_PASSWORD: process.env.POSTGRES_PASSWORD
-};
