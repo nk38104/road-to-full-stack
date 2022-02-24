4c81b98e508c04877ca85c8a7ac2d6ca711baa1c
Nikola Kelava <nikola.kelava94@gmail.com>
2022-02-24 21:54:09 +0100

    Add drum sound for button and keyboard clicks

--- pages/javascript_section/projects/drum_kit/index.js
+++ pages/javascript_section/projects/drum_kit/index.js
@@ -1,9 +1,34 @@
 let drumButtons = document.querySelectorAll("button.drum");
 
 
-function handleClick() {
-    console.log("Clicked");
+function playDrumSound(key) {
+    switch(key) {
+        case "w":
+            new Audio("sounds/tom-1.mp3").play();
+            break;
+        case "a":
+            new Audio("sounds/tom-2.mp3").play();
+            break;
+        case "s":
+            new Audio("sounds/tom-3.mp3").play();
+            break;
+        case "d":
+            new Audio("sounds/tom-4.mp3").play();
+            break;
+        case "j":
+            new Audio("sounds/snare.mp3").play();
+            break;
+        case "k":
+            new Audio("sounds/crash.mp3").play();
+            break;
+        case "l":
+            new Audio("sounds/kick-bass.mp3").play();
+            break;
+        default:
+            console.log(`Pressed key($${key}) not bound to any sound.`);
+    }
 }
 
 
-drumButtons.forEach(btn => btn.addEventListener("click", handleClick));
+drumButtons.forEach(btn => btn.addEventListener("click", (event) => playDrumSound(event.target.textContent)));
+document.addEventListener("keydown", (event) => playDrumSound(event.key));
